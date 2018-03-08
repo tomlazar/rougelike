3d529f78506ccfa901b379944d272ac9164330f8
Tom Lazar <thomas.lazar@marquette.edu>
2018-03-08 12:41:36 -0600

    add corruption bar to HUD

--- src/main/kotlin/games/rougelike/HUD.kt
+++ src/main/kotlin/games/rougelike/HUD.kt
@@ -2,19 +2,34 @@ package games.rougelike
 
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
+import kotlin.math.roundToInt
 
 class HUD {
     companion object {
-        val WIDTH = GAME_WIDTH
-        val HEIGHT = 40.0
+        const val WIDTH = GAME_WIDTH
+        const val HEIGHT = 40.0
 
-        var health = 0.0
+        const val MAX_CORRUPTION = 10
+        var corruption = 6.45
         var score = 0.0
     }
 
     fun render(gc: GraphicsContext) {
         gc.fill = Color.BLACK
-        gc.fillRect(0.0,0.0, WIDTH, HEIGHT)
+        gc.fillRect(0.0, 0.0, WIDTH, HEIGHT)
+
+        val sep = 10.0
+        val barWidth = 20.0
+        var current = sep
+
+        gc.fill = Color.RED
+        for(i in 0..Math.floor(corruption).roundToInt()) {
+            gc.fillRect(current, 2.0, barWidth, 36.0)
+            current += sep + barWidth
+        }
+
+        val remainder = corruption % 1
+        gc.fillRect(current, 2.0, barWidth * remainder, 36.0)
     }
 
     fun update() {
